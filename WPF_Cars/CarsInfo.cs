0ce65ae0e43b105bc7a20f03b807496b711b31ab
AlexandrTolstov <alextav_85@mail.ru>
2018-11-18 11:22:23 +0600

    Com

---
+++ WPF_Cars/CarsInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Cars
+{
+    public class CarsInfo
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public CarsInfo(string name)
+        {
+            Name = name.Remove(name.Length - 4);
+            Path = AppDomain.CurrentDomain.BaseDirectory + "png/" + name;
+        }
+    }
+}
--- WPF_Cars/MainWindow.xaml.cs
+++ WPF_Cars/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Collections;
 
 namespace WPF_Cars
 {
@@ -23,6 +25,57 @@ namespace WPF_Cars
         public MainWindow()
         {
             InitializeComponent();
+
+            DirectoryInfo dir = new DirectoryInfo(@"png");
+            List<CarsInfo> ListOfCars = new List<CarsInfo>();
+            try
+            {
+                foreach (FileInfo files in dir.GetFiles())
+                {
+                    ListOfCars.Add(new CarsInfo(files.Name));
+                }
+            }
+            catch (Exception ex)
+            {
+                Label label = new Label { Content = "Файлы не прочитаны" + ex.Message };
+                WrapCatalogCars.Children.Add(label);
+            }
+
+            Grid [] grids = new Grid[ListOfCars.Count];
+            Label [] labels = new Label[ListOfCars.Count];
+            Image [] image = new Image[ListOfCars.Count];
+            int i = 0;
+
+            foreach (CarsInfo Car in ListOfCars)
+            {
+                image[i] = new Image
+                {
+                    Height = 40,
+                    Source = new BitmapImage(new Uri(Car.Path, UriKind.RelativeOrAbsolute))
+                };
+
+                grids[i] = new Grid
+                {
+                    HorizontalAlignment = HorizontalAlignment.Left,
+                    Margin = new Thickness(10)
+                };
+                grids[i].ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+                grids[i].ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+
+                grids[i].Children.Add(image[i]);
+
+                labels[i] = new Label { Content = Car.Name, Width = 100, Target = grids[i] };
+
+                grids[i].Children.Add(labels[i]);
+
+                Grid.SetColumn(grids[i], 0);
+                Grid.SetColumn(labels[i], 1);
+
+                WrapCatalogCars.Children.Add(grids[i]);
+
+                i++;
+            }
+            i = 0;  
         }
     }
 }
